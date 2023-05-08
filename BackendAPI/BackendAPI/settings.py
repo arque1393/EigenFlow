0944690d6a6837b28e69673b599a0df37e207794
Aritra Ranjan Chowdhury <aritra1393@gmail.com>
2023-05-08 08:28:51 +0530

    update

--- BackendAPI/BackendAPI/settings.py
+++ BackendAPI/BackendAPI/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-%)1h6&zo&#kkuynb&7%735cecogr+x5w4@$%ju#%=90nn)k*0y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.29.174','localhost','127.0.0.1','13.228.225.19','18.142.128.26','54.254.162.138']
+ALLOWED_HOSTS = ['eigen-flow.onrender.com','localhost','127.0.0.1','13.228.225.19','18.142.128.26','54.254.162.138']
 
 
 # Application definition
